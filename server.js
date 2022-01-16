7f3950696171e5220db5a6af40a308784ecd923c
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-15 21:41:16 -0600

    server.js file added, models folder added, routes folder added.

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3006;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", 
+{
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/homeRoutes.js"))
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`)
+})
